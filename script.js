42641dcef29c814952b05748fc72d32912ce80a2
Arlett E. Navea <83241778+abcrlett@users.noreply.github.com>
2023-03-30 15:54:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function onPageLoaded() {
+    // Write your javascript code here
+    console.log("page loaded");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
